bbc49473329755573610d2236e92bbdb701867d6
mithu2649 <mithu2649@gmail.com>
2020-07-24 00:24:54 +0530

    design improvements

--- js/loadAudio.js
+++ js/loadAudio.js
@@ -2,10 +2,10 @@ let audio_input = document.querySelector('#audio_input');
 let load_audio_btn = document.querySelector('#load_audio');
 let audio_source = document.querySelector('#audio_source');
 
-load_audio_btn.addEventListener('click', function(){
+load_audio_btn.addEventListener('click', function() {
     audio_input.click();
 })
-document.querySelector('#audio_input').addEventListener('change', function(){
+document.querySelector('#audio_input').addEventListener('change', function() {
 
     if (audio_input.files.length == 0) {
         console.log('Error : Audio file not selected');
@@ -14,22 +14,22 @@ document.querySelector('#audio_input').addEventListener('change', function(){
     let file = audio_input.files[0];
 
     let reader = new FileReader();
-    reader.addEventListener('loadstart', function () {
+    reader.addEventListener('loadstart', function() {
         console.log('Loading Audio');
     });
 
-    reader.addEventListener('load', function (e) {
+    reader.addEventListener('load', function(e) {
         audio.setAttribute('controls', true);
         document.getElementById('song_info').innerHTML = audio_input.files[0].name;
         let audio_src = e.target.result;
         audio.setAttribute('src', audio_src);
     });
 
-    reader.addEventListener('error', function () {
+    reader.addEventListener('error', function() {
         console.log('Error : Failed to load file');
     });
 
-    reader.addEventListener('progress', function (e) {
+    reader.addEventListener('progress', function(e) {
         if (e.lengthComputable == true) {
             let percent_read = Math.floor((e.loaded / e.total) * 100);
             console.log(percent_read + '% Audio loaded');
@@ -37,8 +37,8 @@ document.querySelector('#audio_input').addEventListener('change', function(){
     });
 
     reader.readAsDataURL(file);
- 
-    
- 
-    
+
+
+
+
 })
--- js/viewLyrics.js
+++ js/viewLyrics.js
@@ -10,7 +10,7 @@ let next_lyrics = document.querySelector('#next_line');
 
 next_lyrics.innerHTML = 'Enjoy the music!'
 
-audio.addEventListener('timeupdate', function () {
+audio.addEventListener('timeupdate', function() {
     playback_time = audio.currentTime;
     // .toString().toTime();
     // document.querySelector('#timestamp').innerHTML = playback_time;
@@ -20,11 +20,11 @@ audio.addEventListener('timeupdate', function () {
 });
 
 
-read_btn.addEventListener('click', function () {
+read_btn.addEventListener('click', function() {
     file_input.click();
 });
 
-file_input.addEventListener('change', function () {
+file_input.addEventListener('change', function() {
     if (file_input.files.length == 0) {
         console.log('Error : No file selected');
         return;
@@ -32,23 +32,23 @@ file_input.addEventListener('change', function () {
     let file = file_input.files[0];
 
     let reader = new FileReader();
-    reader.addEventListener('loadstart', function () {
+    reader.addEventListener('loadstart', function() {
         console.log('Reading Lyrics...');
     });
 
-    reader.addEventListener('load', function (e) {
+    reader.addEventListener('load', function(e) {
         let text = e.target.result;
         lyrics = cleanLyrics(text.split('\n'), "", 12);
-        lyrics.forEach(function(item){
+        lyrics.forEach(function(item) {
             console.log(item)
         })
     });
 
-    reader.addEventListener('error', function () {
+    reader.addEventListener('error', function() {
         console.log('Error : Failed to read file');
     });
 
-    reader.addEventListener('progress', function (e) {
+    reader.addEventListener('progress', function(e) {
         if (e.lengthComputable == true) {
             let percent_read = Math.floor((e.loaded / e.total) * 100);
             console.log(percent_read + '% read');
@@ -59,48 +59,48 @@ file_input.addEventListener('change', function () {
 });
 
 
-String.prototype.toSeconds = function () {
+String.prototype.toSeconds = function() {
     let mmss = this.substring(1, 9);
     let a = mmss.split(':');
     let min_to_sec = parseFloat(a[0] * 60);
     let sec = parseFloat(a[1]);
 
     let seconds = min_to_sec + sec;
-    return seconds.toFixed(2); 
+    return seconds.toFixed(2);
 }
 
 function cleanLyrics(arr, value, range) {
     let i = 0;
     while (i < arr.length) {
-      if (arr[i].length < range || arr[i]==value) {
-        arr.splice(i, 1);
-      } else {
-        const regex = /\[(.*?)\]/g;
-        let match = arr[i].match(regex);
-        let timestamp;
-        if(!match){
-            console.log('unreadable line')
-        }else{
-            timestamp = match[0].toSeconds();
+        if (arr[i].length < range || arr[i] == value) {
+            arr.splice(i, 1);
+        } else {
+            const regex = /\[(.*?)\]/g;
+            let match = arr[i].match(regex);
+            let timestamp;
+            if (!match) {
+                console.log('unreadable line')
+            } else {
+                timestamp = match[0].toSeconds();
+            }
+
+            arr[i] = arr[i].replace(regex, '[' + padEvenly(timestamp) + ']');
+            ++i;
         }
-        
-        arr[i] = arr[i].replace(regex, '[' + padEvenly(timestamp) + ']');
-        ++i;
-      }
     }
     return arr;
-  }
+}
 
-  function displayLyricsAt(playback_time){
+function displayLyricsAt(playback_time) {
     let i = 0;
-    while(i < lyrics.length){
+    while (i < lyrics.length) {
         playback_time = parseFloat(playback_time)
         let lyrics_timestamp = parseFloat(lyrics[i].substring(1, 8));
-        let offset = 0.800;
-        if(playback_time + offset >= lyrics_timestamp){
+        let offset = 0.400;
+        if (playback_time + offset >= lyrics_timestamp) {
 
             current = lyrics[i].substring(8).replace(/\[(.*?)\]/g, '');
-            next = lyrics[i+1].substring(8).replace(/\[(.*?)\]/g, '');
+            next = lyrics[i + 1].substring(8).replace(/\[(.*?)\]/g, '');
 
             current_lyrics.innerHTML = current;
             next_lyrics.innerHTML = next;
@@ -108,14 +108,14 @@ function cleanLyrics(arr, value, range) {
 
         i++;
     }
-  }
+}
 
-  function padEvenly(n) {
-    if(n < 10) {
+function padEvenly(n) {
+    if (n < 10) {
         return '00' + n;
-    }else if(n < 100){
+    } else if (n < 100) {
         return '0' + n;
-    }else{
+    } else {
         return n;
     }
-  }
+}
